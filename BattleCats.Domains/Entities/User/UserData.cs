be5a465d62c7379be62843e9ffa830ce407a1edc
radicq <radaciornii15@gmail.com>
2026-05-05 12:38:51 +0300

    Refactor: align UserData with eUShop pattern

--- BattleCats.Domains/Entities/User/UserData.cs
+++ BattleCats.Domains/Entities/User/UserData.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BattleCats.Domains.Enums;
+using BattleCats.Domain.Enums;
 
-namespace BattleCats.Domains.Entities.User
+namespace BattleCats.Domain.Entities.User
 {
+    /// <summary>
+    /// Сущность пользователя сайта Cat Base Shop.
+    /// Хранит логин, email, пароль (БУДЕТ хэширован в Лабе 5).
+    /// Поле Role будет добавлено в Лабе 5 для разграничения прав (User/Admin).
+    /// </summary>
     public class UserData
     {
         [Key]
@@ -18,23 +17,22 @@ namespace BattleCats.Domains.Entities.User
 
         [Required]
         [StringLength(20)]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         [Required]
         [StringLength(50, MinimumLength = 8)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [Required]
         [StringLength(50)]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
-
-        public string Contacts { get; set; }
+        public string? Contacts { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
 
-        public GenderTypes Gender { get; set; }
+        public GenderTypes Gender { get; set; } = GenderTypes.NotSpecified;
     }
 }
