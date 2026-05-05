b4391a756bd0be4d0be05a978cdcf8e077d197a7
markl666-nat <marinskijm@inbox.ru>
2026-05-05 11:23:48 +0300

    Refactor: rebuild ItemCategory entity for Battle Cats

--- BattleCats.Domains/Entities/Product/ItemCategory.cs
+++ BattleCats.Domains/Entities/Product/ItemCategory.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BattleCatsStore.Domains.Entities.Products
+namespace BattleCats.Domain.Entities.Product
 {
+    /// <summary>
+    /// Категория товара в магазине Battle Cats.
+    /// Возможные значения (хранятся как записи в БД через seed):
+    /// "Cat Units", "Base Upgrades", "Buffs", "Gacha".
+    /// Связь с BattleItem — many-to-one (одна категория содержит много товаров).
+    /// </summary>
     public class ItemCategory
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int CategoryId { get; set; } // Сменили Id на CategoryId
+        public int Id { get; set; }
 
-        public string Title { get; set; } // Сменили Name на Title (например: "Normal Cats", "Uber Rare", "Plushies")
+        /// <summary>
+        /// Название категории. Совпадает со значениями на фронте (types.ts):
+        /// "Cat Units", "Base Upgrades", "Buffs", "Gacha".
+        /// </summary>
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
     }
 }
