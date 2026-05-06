b91e0d1c639a971c43e6e3fc48a9fcc4d9de8ec6
markl666-nat <marinskijm@inbox.ru>
2026-05-06 17:17:39 +0300

    Refactor(bl): align IBattleItem with reference IProduct interface

--- BattleCats.BusinessLogic/Interface/IBattleItem.cs
+++ BattleCats.BusinessLogic/Interface/IBattleItem.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BattleCatsStore.Domains.Models.Products;
 using BattleCats.Domains.Models.Base;
 using BattleCats.Domains.Models.Product;
 
-namespace BattleCatsStore.BusinessLogic.Interface
+namespace BattleCats.BusinessLogic.Interface
 {
     public interface IBattleItem
     {
-        // CRUD: Read (All)
-        List<BattleItemDto> GetAllCombatUnits();
-
-        // CRUD: Read (Single)
-        BattleItemDto? GetUnitByInternalId(int id);
-
-        // CRUD: Update
-        ActionResponse UpdateItemRecord(BattleItemDto item);
-
-        // CRUD: Delete
-        ActionResponse RemoveItemFromCatalog(int id);
-
-        // CRUD: Create
-        ActionResponse RegisterNewItem(BattleItemDto item);
+        List<BattleItemDto> GetAllBattleItemsAction();
+        BattleItemDto GetBattleItemByIdAction(int id);
+        ActionResponse ResponceBattleItemCreateAction(BattleItemDto item);
+        ActionResponse ResponceBattleItemUpdateAction(BattleItemDto item);
+        ActionResponse ResponceBattleItemDeleteAction(int id);
     }
 }
