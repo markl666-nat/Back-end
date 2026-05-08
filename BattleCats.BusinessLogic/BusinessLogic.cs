445bb8ce42df9967a13a699a41167432b700723d
markl666-nat <marinskijm@inbox.ru>
2026-05-08 21:53:18 +0300

    Refactor(bl): expose IRegisterActions and IOrderAction in facade

--- BattleCats.BusinessLogic/BusinessLogic.cs
+++ BattleCats.BusinessLogic/BusinessLogic.cs
@@ -1,9 +1,15 @@
 ﻿using BattleCats.BusinessLogic.Functions.Auth;
+using BattleCats.BusinessLogic.Functions.Order;
 using BattleCats.BusinessLogic.Functions.Products;
 using BattleCats.BusinessLogic.Interface;
 
 namespace BattleCats.BusinessLogic
 {
+    /// <summary>
+    /// Главный фасад BusinessLogic.
+    /// API-контроллеры создают экземпляр этого класса и через геттеры получают
+    /// нужный интерфейс. Каждый Get* возвращает свежий Flow-объект.
+    /// </summary>
     public class BusinessLogic
     {
         public BusinessLogic() { }
@@ -13,9 +19,19 @@ namespace BattleCats.BusinessLogic
             return new AuthFlow();
         }
 
+        public IRegisterActions GetRegisterActions()
+        {
+            return new RegisterFlow();
+        }
+
         public IBattleItem GetBattleItemActions()
         {
             return new BattleItemFlow();
+        }
+
+        public IOrderAction GetOrderActions()
+        {
+            return new OrderFlow();
         }
     }
 }
