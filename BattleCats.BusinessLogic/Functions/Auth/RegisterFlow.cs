da5acc08c3ae5fe123d0c8de2ca9466d7bea630f
radicq <radaciornii15@gmail.com>
2026-05-08 21:42:51 +0300

    Add(bl): RegisterFlow as thin wrapper over RegisterActions

---
+++ BattleCats.BusinessLogic/Functions/Auth/RegisterFlow.cs
@@ -0,0 +1,18 @@
+﻿using BattleCats.BusinessLogic.Core.Auth;
+using BattleCats.BusinessLogic.Interface;
+using BattleCats.Domains.Models.Base;
+using BattleCats.Domains.Models.User;
+
+namespace BattleCats.BusinessLogic.Functions.Auth
+{
+    /// <summary>
+    /// Поток обработки регистрации. Тонкая обёртка над RegisterActions.
+    /// </summary>
+    public class RegisterFlow : RegisterActions, IRegisterActions
+    {
+        public ResponceAction RegisterActionFlow(UserRegisterDto uReg)
+        {
+            return RegisterUserExecution(uReg);
+        }
+    }
+}
