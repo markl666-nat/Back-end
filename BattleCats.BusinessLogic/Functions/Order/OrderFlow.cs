db965423a3a5db6825c11c5e3ee05d5d81782a07
radicq <radaciornii15@gmail.com>
2026-05-08 21:51:59 +0300

    Add(bl): IOrderAction interface and OrderFlow wrapper

---
+++ BattleCats.BusinessLogic/Functions/Order/OrderFlow.cs
@@ -0,0 +1,38 @@
+﻿using BattleCats.BusinessLogic.Core.Order;
+using BattleCats.BusinessLogic.Interface;
+using BattleCats.Domains.Models.Base;
+using BattleCats.Domains.Models.Order;
+
+namespace BattleCats.BusinessLogic.Functions.Order
+{
+    /// <summary>
+    /// Поток операций с заказами. Тонкая обёртка над OrderAction.
+    /// </summary>
+    public class OrderFlow : OrderAction, IOrderAction
+    {
+        public List<OrderDto> GetAllOrders()
+        {
+            return GetAllOrdersAction();
+        }
+
+        public OrderDto GetOrderItem(int id)
+        {
+            return GetOrderByIdAction(id);
+        }
+
+        public ResponceAction CreateOrder(OrderDto order)
+        {
+            return CreateOrderAction(order);
+        }
+
+        public ResponceMsg UpdateOrder(OrderDto order)
+        {
+            return UpdateOrderAction(order);
+        }
+
+        public ResponceMsg DeleteOrder(int id)
+        {
+            return DeleteOrderAction(id);
+        }
+    }
+}
---
+++ BattleCats.BusinessLogic/Interface/IOrderAction.cs
@@ -0,0 +1,19 @@
+﻿using BattleCats.Domains.Models.Base;
+using BattleCats.Domains.Models.Order;
+
+namespace BattleCats.BusinessLogic.Interface
+{
+    /// <summary>
+    /// Интерфейс CRUD-операций с заказами.
+    /// Create возвращает ResponceAction (с Id нового заказа).
+    /// Update/Delete возвращают ResponceMsg (без Id).
+    /// </summary>
+    public interface IOrderAction
+    {
+        List<OrderDto> GetAllOrders();
+        OrderDto GetOrderItem(int id);
+        ResponceAction CreateOrder(OrderDto order);
+        ResponceMsg UpdateOrder(OrderDto order);
+        ResponceMsg DeleteOrder(int id);
+    }
+}
