b1804b65e3145c9b855f40a6bf4f4ccd4ee7deeb
radicq <radaciornii15@gmail.com>
2026-05-08 21:07:46 +0300

    Add(domains): create Order folder and OrderStatus enum

---
+++ BattleCats.Domains/Entities/Order/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace BattleCats.Domains.Entities.Order
+{
+    /// <summary>
+    /// Статус заказа в магазине Cat Base Shop.
+    /// "Aceepted" — опечатка препода (правильно "Accepted"), оставляем для 1-в-1.
+    /// </summary>
+    public enum OrderStatus
+    {
+        None,
+        Declined,
+        Aceepted,
+        Validating,
+        Paid,
+        Delivered,
+        Refund
+    }
+}
